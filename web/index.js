2bba4d4a3174ba299adfb28d529dbacf9371a7e1
Fidel Thomet <fidel@Fidels-MacBook-Air.local>
2016-06-02 20:10:21 +0200

    clean code

--- web/index.js
+++ web/index.js
@@ -6,6 +6,17 @@ d3.select(document).on("DOMContentLoaded", () => {
 	let x = d3.time.scale().range([0, width])
 	let y = d3.scale.linear().range([height, 0])
 
+	let xAxis = d3.svg.axis().scale(x)
+		.orient("bottom")
+		.ticks(5)
+
+	let yAxis = d3.svg.axis().scale(y)
+		.orient("right")
+		.ticks(5)
+		.tickSize(width)
+		.tickFormat(d3.format(".0%"))
+
+
 	let line = (gender, data) => {
 		var l = d3.svg.line()
 			.x((d) => {
@@ -46,119 +57,49 @@ d3.select(document).on("DOMContentLoaded", () => {
 			.key(d => d.char)
 			.entries(data)
 
-		let p = d3.select(".container").selectAll(".group").data(dataNest)
-		let g = p.enter().append("g").attr("class", "group")
+		let container = svg.selectAll(".group").data(dataNest)
+		let groups = container.enter().append("g").attr("class", "group")
 
-		g.append("path").attr("class", "line1")
-			.attr("d", (d) => {
-				return line("a", d.values)
-			})
+		groups.append("path").attr("class", "line1")
+			.attr("d", d => line("a", d.values))
 			.style({
 				stroke: color("a"),
 				fill: "none",
 				opacity: .5
 			})
 
-		g.append("path").attr("class", "line2")
-			.attr("d", (d) => {
-				return line("a", d.values)
-			})
-			.style({
-				stroke: color("a"),
-				fill: "none",
-				opacity: .0
-			})
+		svg.append("g").attr("class", "temp")
 
-		g.append("path").attr("class", "line3")
-			.attr("d", (d) => {
-				return line("f", d.values)
+		let xa = svg.append("g")
+			.attr({
+				class: "x axis",
+				transform: "translate(0," + height + ")"
 			})
-			.style({
-				stroke: color("f"),
-				fill: "none",
-				opacity: .0
-			})
-
-		g.append("path").attr("class", "line4")
-			.attr("d", (d) => {
-				return line("m", d.values)
-			})
-			.style({
-				stroke: color("m"),
-				fill: "none",
-				opacity: .0
-			})
-
-
-		var xAxis = d3.svg.axis().scale(x)
-			.orient("bottom").ticks(5)
-
-		var yAxis = d3.svg.axis().scale(y)
-			.tickSize(width)
-			.ticks(5)
-			.orient("right")
-			.tickFormat(d3.format(".0%"))
-
-		var xa = svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
 			.call(xAxis)
-
 		xa.selectAll("line").filter((d, i) => xa.selectAll("line")[0].length - 1 === i).attr({
-			"x2": -1,
-			"x1": -1
+			x2: -1,
+			x1: -1
 		})
 		xa.append("text")
-			.attr("text-anchor", "end")
-			.attr("transform", "translate(" + (width) + ",36)")
+			.attr({
+				"text-anchor": "end",
+				transform: "translate(" + (width) + ",36)"
+			})
 			.text("Year")
 
-		var ya = svg.append("g")
+		let ya = svg.append("g")
 			.attr("class", "y axis")
-			.attr("transform", "translate(0,0)")
 			.call(yAxis)
-
 		ya.selectAll("text")
-			.attr("x", 0)
-			.attr("y", -7)
-
+			.attr({
+				x: 0,
+				y: -7
+			})
 		ya.append("text")
 			.attr("text-anchor", "start")
-			.attr("transform", "translate(0,0)")
 			.text("Names starting with given letter")
 
-		d3.select("#filter #f").on("click", () => {
-			d3.selectAll("#filter span").attr("class", "")
-			d3.selectAll("#filter #f").attr("class", "selected")
-			transition("f")
-		})
-		d3.select("#filter #m").on("click", () => {
-			d3.selectAll("#filter span").attr("class", "")
-			d3.selectAll("#filter #m").attr("class", "selected")
-			transition("m")
-		})
-		d3.select("#filter #a").on("click", () => {
-			d3.selectAll("#filter span").attr("class", "")
-			d3.selectAll("#filter #a").attr("class", "selected")
-			transition("a")
-		})
-
-		d3.selectAll(".group path").on("mouseenter", (d, i) => {
-			highlight(d.key)
-		})
-
-		d3.selectAll(".group path").on("mouseleave", reset)
-
-		d3.selectAll("#description span").on("mouseenter", () => {
-			highlight(d3.select(d3.event.target).text())
-		})
-
-		d3.selectAll("#description span").on("mouseleave", reset)
-
-		d3.select("body").on("keydown", () => {
-			if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(String.fromCharCode(d3.event.which)) !== -1)
-				highlight(String.fromCharCode(d3.event.which))
-		})
+		events()
 	})
 
 	let transition = (gender) => {
@@ -171,32 +112,58 @@ d3.select(document).on("DOMContentLoaded", () => {
 	let highlight = (key) => {
 		reset()
 		key = key.toLowerCase()
-		d3.selectAll(".group path").filter((d, i) => {
-			return key === d.key
-		}).style({
-			"stroke-width": 2,
-			"opacity": .6
-		})
-		d3.selectAll(".group path.line1").filter((d, i) => {
+		svg.selectAll(".group path").filter((d, i) => {
 			return key === d.key
 		}).style({
 			"stroke-width": 4,
 			"opacity": 1
+		}).each((d, i) => {
+			svg.selectAll(".temp").append("path").attr("class", "line1")
+				.attr("d", () => line("f", d.values))
+				.style({
+					stroke: color("f"),
+					fill: "none",
+					opacity: .5
+				})
+			svg.selectAll(".temp").append("path").attr("class", "line1")
+				.attr("d", () => line("m", d.values))
+				.style({
+					stroke: color("m"),
+					fill: "none",
+					opacity: .5
+				})
 		})
-		d3.selectAll("header span").text(key.toUpperCase())
+
+		svg.selectAll("header span").text(key.toUpperCase())
 	}
 
-	reset = () => {
-		d3.selectAll(".group path").style({
-			"stroke-width": 1,
-			"opacity": .0
-		})
-		d3.selectAll(".group path.line1").style({
+	let reset = () => {
+		svg.selectAll(".group path").style({
 			"stroke-width": 1,
 			"opacity": .5
 		})
-		d3.select("header span").text("?")
+		svg.select("header span").text("?")
+		svg.select(".temp").selectAll("*").remove()
 	}
 
 	let color = g => g === "a" ? "#B61489" : (g === "f" ? "#EE3E47" : "#5B46F6")
+
+	let events = () => {
+		d3.selectAll("#filter span").on("click", () => {
+			d3.selectAll("#filter span").attr("class", "")
+			d3.select(d3.event.target).attr("class", "selected")
+			transition(d3.event.target.id)
+		})
+
+		svg.selectAll(".group path").on("mouseenter", d => highlight(d.key))
+		svg.selectAll(".group path").on("mouseleave", reset)
+
+		svg.selectAll("#description span").on("mouseenter", () => highlight(svg.select(d3.event.target).text()))
+		svg.selectAll("#description span").on("mouseleave", reset)
+
+		svg.select("body").on("keydown", () => {
+			if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(String.fromCharCode(d3.event.which)) !== -1)
+				highlight(String.fromCharCode(d3.event.which))
+		})
+	}
 })
